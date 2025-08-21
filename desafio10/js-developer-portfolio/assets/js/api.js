1a19a4fe3b483d3e8dfa4a9a531c996ca00b03bd
Sabrina Souza <sabrinasouza.dev@gmail.com>
2025-08-21 11:22:06 -0300

    chore: add dados do json

--- desafio10/js-developer-portfolio/assets/js/api.js
+++ desafio10/js-developer-portfolio/assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/SabrinaZ8/santander-frontend-2025/refs/heads/main/desafio10/js-developer-portfolio/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
